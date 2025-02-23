ac87d940652bc60bcb3ea3089be2261a1fbfd64c
Nikita Sanjay Yadav <nikitayadav2102@gmail.com>
2025-02-23 11:36:36 +0530

    Add files via upload

---
+++ practice jse code/datatypes/String datatype/concatMethod.java
@@ -0,0 +1,9 @@
+public class concatMethod {
+    public static void main(String arg[])
+    {
+        String str="hello";
+        String str1="Hi";
+        System.out.println(str.concat(" "+str1+" "+str+" ")+str.concat(" nikita"));
+    } 
+    
+}
