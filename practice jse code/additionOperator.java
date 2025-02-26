33f47941e66ad5dd1c363ab2a3cac867014a775b
Nikita Sanjay Yadav <nikitayadav2102@gmail.com>
2025-02-26 12:12:22 +0530

    Add files via upload

---
+++ practice jse code/additionOperator.java
@@ -0,0 +1,11 @@
+public class additionOperator {
+    public static void main(String arg[])
+    {
+        int a=10;
+        int b=20;
+        System.out.println("The value is = "+a+b);
+        System.out.println("The value is = "+(a+b));
+        
+    }
+    
+}
